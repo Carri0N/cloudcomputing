c3a5ca4f99228f44d7b770ecee6b8be3c0b7965f
Adrian <adrian_schwab@web.de>
2018-10-18 12:38:18 +0200

    changed sending messages format

--- server/server.js
+++ server/server.js
@@ -5,31 +5,32 @@ const server = http.Server(app);
 const socketIO = require('socket.io');
 const io = socketIO(server);
 const port = process.env.PORT || 3000;
+const moment = require('moment');
 
 var userlist = [];
 
 io.on('connection', (socket) => {
 
-  socket.on('join', function(info) {
-    socket.join(info.chat);
-    socket.nickname = info.user;
-    userlist.push(info.user);
-    socket.broadcast.to(info.chat).emit('message', {timestamp: "TODO", user: "Server", msg: socket.nickname + ' has joined'}); //send msg to e1 in same chat
-    console.log(info.user + ' joined ' + info.chat);
-  })
-
-  socket.on('message', function(info) {
-    io.in(info.chat).emit('message', info.msg);
-  })
-
   socket.on('disconnect', function(){
     userlist = userlist.filter(x => x != socket.nickname);
     console.log(socket.nickname + ' disconnected');
   })
-  
-  socket.on('list', function(){
-    console.log('list request');
-  }) 
+
+  socket.on('join', function(info) {
+    socket.join(info.chat);
+    socket.nickname = info.user;
+    userlist.push(info.user);
+    socket.broadcast.to(info.chat).emit('message', {timestamp: moment().format('hh:mm A'), user: "Server", msg: socket.nickname + ' has joined'}); //send msg to e1 in same chat
+    console.log(info.user + ' joined ' + info.chat);
+  })
+
+  socket.on('message', function(info) {
+    io.in(info.chat).emit('message', {timestamp: moment().format('hh:mm A') ,user: socket.nickname, msg: info.msg});
+  })
+
+  socket.on('file', function() {
+    console.log("file");
+  })
 
 });
 
